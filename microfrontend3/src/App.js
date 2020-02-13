bc65133d2cf34d6df514a8a3724cbb1f46857482
nidhinkumar <nidhinkumar@codingtown.com>
2020-02-13 19:21:32 +0530

    add webcomponents

---
+++ microfrontend3/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  state = {
+    hello: "React 0.14 Web Component",
+    title: "Bodyguard"
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Title: {this.props.title}</h3>
+        <p>Greeting: {this.state.hello}</p>
+      </div>
+    );
+  }
+}
